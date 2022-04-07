4a214e072a0faf3540fd1b01526e5dc7051f7cb5
ISHIFT <kkh47406@naver.com>
2022-04-07 09:35:52 +0900

    [04.07] Blog Update & Delete

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,16 +3,22 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -26,6 +32,37 @@ public class DummyControllerTest {
 	
 	@Autowired // 의존성 주입
 	private UserRepository userRepository;
+	
+	@DeleteMapping("/dummy/user/{id}")
+	public String delete(@PathVariable int id) {
+		
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {  // DB에 존재하지 않는 데이터에 접근할 때 발생하는 예외 
+			return "삭제에 실패하였습니다. 해당 id는 DB에 없습니다.";
+		}
+		
+		return "삭제되었습니다. id : " + id;
+	}
+	
+	// save 함수는 id를 전달하면 값이 있을 땐 update, 값이 없을 땐 insert
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		 log.info("id : " + id);
+		 log.info("password : " +requestUser.getPassword());
+		 log.info("email : " +requestUser.getEmail());
+		 
+		 User user = userRepository.findById(id).orElseThrow(() -> {
+			 return new IllegalArgumentException("수정에 실패하였습니다.");
+		 });
+		 
+		 user.setPassword(requestUser.getPassword());
+		 user.setEmail(requestUser.getEmail());
+		 
+		 // 더티 체킹
+		 return user;
+	}
 	
 	@GetMapping("/dummy/users")
 	public List<User> list(){
