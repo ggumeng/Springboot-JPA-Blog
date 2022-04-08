ed8e65aaea4c13174a0ba135a8b5e40ae7c2e8db
ISHIFT <kkh47406@naver.com>
2022-04-08 15:42:14 +0900

    [04.08] Blog login process with spring security

--- src/main/java/com/cos/blog/config/SecurityConfig.java
+++ src/main/java/com/cos/blog/config/SecurityConfig.java
@@ -1,22 +1,37 @@
 package com.cos.blog.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.cos.blog.config.auth.PrincipalDetailService;
+
 // Bean 등록 : 스프링 컨테이너에서 객체를 관리할 수 있게 하는 것
 @Configuration // bean 등록 (IoC 관리)
 @EnableWebSecurity // 시큐리티 필터가 등록이 된다.
 @EnableGlobalMethodSecurity(prePostEnabled = true) // 특정 주소로 접근하면 권한 및 인증을 미리 체크)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Autowired
+	private PrincipalDetailService principalDetailService;
 
 	@Bean // IoC 가능
 	public BCryptPasswordEncoder encodePWD() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	// 시큐리티가 대신 로그인 해주는데 password를 가로채기를 하는데
+	// 해당 password 가 뭐로 해쉬가 되어 회원가입이 되었는지 알아야
+	// 같은 해쉬로 암호화해서 DB에 있는 해쉬랑 비교할 수 있음.
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
 	}
 	
 	@Override
@@ -30,7 +45,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		  	.authenticated()
 		  .and()
 			.formLogin()
-			.loginPage("/auth/loginForm");
+			.loginPage("/auth/loginForm")
+			.loginProcessingUrl("/auth/loginProc")
+			.defaultSuccessUrl("/"); // spring security가 해당 주소로 요청오는 로그인을 가로챈다
 	}
-	
 }
--- src/main/java/com/cos/blog/repository/UserRepository.java
+++ src/main/java/com/cos/blog/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.cos.blog.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -8,7 +10,8 @@ import com.cos.blog.model.User;
 // DAO
 // 자동으로 bean 등록이 된다. @Repository 생략 가능
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+	// select * from user where username = ?
+	Optional<User> findByUsername(String username);
 }
 
 //JPA Naming 쿼리
